acd6d332329219d7f7e55aca4a9cb2be78420d68
Sophie Lavigne <Sophie.Lavigne@yahoo.com>
2021-03-24 18:49:02 -0400

    Added first draft position stack implementation

--- src/main/java/com/revature/repository/PositionRepositoryImpl.java
+++ src/main/java/com/revature/repository/PositionRepositoryImpl.java
@@ -10,6 +10,9 @@ import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.revature.models.Game;
+import com.revature.models.Portfolio;
+import com.revature.models.Position;
 import com.revature.models.User;
 
 @Repository("positionRepository")
@@ -23,28 +26,12 @@ public class PositionRepositoryImpl implements PositionRepository{
 	public PositionRepositoryImpl() {
 		logger.trace("Injection session factory bean");
 	}
-}
-
-/*
- * Sample Method Stubs below (from UserRepositoryImpl()).
 	
 	@Override
-	public void register(User user) {
-		logger.info("Attempting to register user.");
-		sessionFactory.getCurrentSession().save(user);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	public List<User> getAllUsers() {
-		logger.info("Attempting to list all users.");
-		return sessionFactory.getCurrentSession().createCriteria(User.class).list();
-	}
-
-	@Override
-	public User getUser(String username) {
+	public Position getPosition(User user, Game game, String stockSymbol) {
+		Portfolio port = PortfolioService.getPortfolio(user, game);
 		try {
-			return (User) sessionFactory.getCurrentSession().createCriteria(User.class).add(Restrictions.like("username", username))
+			return (Position) sessionFactory.getCurrentSession().createCriteria(Position.class).add(Restrictions.like("portfolio_id", port.getPortfolioId())).add("stock_symbol", stockSymbol)
 					.list().get(0);
 		} catch (IndexOutOfBoundsException e) {
 			logger.debug(e);
@@ -52,5 +39,18 @@ public class PositionRepositoryImpl implements PositionRepository{
 		}
 	}
 
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Position> getPositionsForUser(User user, Game game) {
+		Portfolio port = PortfolioService.getPortfolio(user, game);
+		return (List<Position>) sessionFactory.getCurrentSession().createCriteria(User.class).add(Restrictions.like("portfolio_id", port.getPortfolioId()))
+				.list(); //Unchecked cast to List<Position>.
+	}
+	
+	@Override
+	public void createOrUpdatePosition(Position position) {
+		sessionFactory.getCurrentSession().saveOrUpdate(position);
+	}
+	
+	
 }
- */
--- src/main/java/com/revature/service/PositionService.java
+++ src/main/java/com/revature/service/PositionService.java
@@ -1,4 +1,4 @@
-package com.revature.repository;
+package com.revature.service;
 
 import java.util.List;
 
@@ -6,8 +6,7 @@ import com.revature.models.Game;
 import com.revature.models.Position;
 import com.revature.models.User;
 
-public interface PositionRepository {
-	
+public interface PositionService {
 	public List<Position> getPositionsForUser(User user, Game game);
 
 	Position getPosition(User user, Game game, String stockSymbol);
